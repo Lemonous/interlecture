b03c7621d329beeded8a2e99ad334f4df932d636
plotkin1996 <plotkin@null.net>
2017-02-09 17:11:48 +0100

    Brought back chat component, minor changes.

--- interlecture/playground/consumers.py
+++ interlecture/playground/consumers.py
@@ -2,6 +2,7 @@ from channels import Group
 
 def ws_add(message):
   message.reply_channel.send({"accept": True})
+  message.reply_channel.send({"text": "Welcome"})
   Group("chat").add(message.reply_channel)
 
 def ws_message(message):
--- interlecture/react/main.jsx
+++ interlecture/react/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import Chat from './test.jsx';
+import Chat from './chat.jsx';
 
 function clickButton() {
   console.log("button clicked");
