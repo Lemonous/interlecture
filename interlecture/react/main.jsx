5a2aad23e27186bc5ec6fead5ea61efe0937d0af
plotkin1996 <plotkin@null.net>
2017-02-09 03:23:44 +0100

    Simple chat application/WS example.

--- interlecture/react/main.jsx
+++ interlecture/react/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
-import Test from './test.jsx';
+import Chat from './test.jsx';
 
 function clickButton() {
   console.log("button clicked");
 }
 
 const Main = () => (
-  <Test color='#459CD4' size='40px' text='Hello!' clickButton={clickButton}/>
+  <div style={{margin: '5pt 5%'}}>
+    <Chat server={"ws://" + window.location.host}/>
+  </div>
 );
 
 render(<Main/>, document.getElementById('main'))
--- interlecture/react/test.jsx
+++ interlecture/react/test.jsx
@@ -1,16 +1,59 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Panel, Form, FormGroup, FormControl, InputGroup, Button } from 'react-bootstrap';
 
-const test = props => (
-  <div style={{
-      backgroundColor: props.color,
-      fontSize: props.size,
-    }}>
-    {props.text}
-    <Button onClick={() => props.clickButton()}>
-      click me plz
-    </Button>
-  </div>
-);
+class Chat extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={msg: '',text:[]};
+        
+        this.handleInput=this.handleInput.bind(this);
+        this.handleSubmit=this.handleSubmit.bind(this);
+        
+        this.socket=new WebSocket(this.props.server);
+        this.handleOpen=this.handleOpen.bind(this);
+        this.socket.onopen=this.handleOpen;
+        this.handleRecieve=this.handleRecieve.bind(this);
+        this.socket.onmessage=this.handleRecieve;
+      }
+    
+    
+    handleRecieve(event) {
+        this.outputString(event.data);
+      }
+    
+    handleOpen(event) {
+        this.outputString("Connected to "+this.props.server);
+      }
+    
+    handleInput(event) {
+        this.setState({msg: event.target.value,text:this.state.text});
+      }
+    
+    outputString(s) {
+        this.setState({msg: '',text:this.state.text.concat(<li key={this.state.text.length}> {s} </li>)});
+      }
+    
+    handleSubmit(event) {
+        event.preventDefault();
+        if(this.state.msg!='') this.socket.send(this.state.msg);
+      }
+    
+    render() {return(
+        <div>
+          <Panel>
+            <ul>{this.state.text}</ul>
+          </Panel>
+          <Form onSubmit={this.handleSubmit}>
+            <InputGroup>
+              <FormControl type="text" onChange={this.handleInput}
+                  value={this.state.msg} placeholder="Enter message"/>
+              <InputGroup.Button> <Button onClick={this.handleSubmit}>
+                Send
+              </Button> </InputGroup.Button>
+            </InputGroup>
+          </Form>
+        </div>
+      );}
+  }
 
-export default test;
+export default Chat;
