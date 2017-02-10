bddd925f41c35e6ba164ddce7364e25dd21041bc
Kristian Trandem <kkrorkk@gmail.com>
2017-02-10 16:23:12 +0100

    refactor and add docstring

--- interlecture/playground/consumers.py
+++ interlecture/playground/consumers.py
@@ -1,12 +1,16 @@
 from channels import Group
 
+
 def ws_add(message):
-  message.reply_channel.send({"accept": True})
-  message.reply_channel.send({"text": "Welcome"})
-  Group("chat").add(message.reply_channel)
+    message.reply_channel.send({"accept": True})
+    message.reply_channel.send({"text": "Welcome"})
+    Group("chat").add(message.reply_channel)
+
 
 def ws_message(message):
-  Group("chat").send({"text":"> "+message.content["text"]})
+    """Incoming messages are handled here - see routing.py."""
+    Group("chat").send({"text": "> " + message.content["text"]})
+
 
 def ws_disconnect(message):
     Group("chat").discard(message.reply_channel)
