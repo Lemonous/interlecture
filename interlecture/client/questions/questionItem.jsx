5253b77edf86b852fbdfa37a708934844a21854d
Erlend Morthen <erlend.morthen@gmail.com>
2017-03-23 11:27:34 +0100

    allow showing of  a correct questionitem

--- interlecture/client/questions/questionItem.jsx
+++ interlecture/client/questions/questionItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Grid, Row, Col } from 'react-bootstrap';
 import FontAwesome from 'react-fontawesome';
 
 
@@ -19,15 +19,48 @@ const QuestionItem = ({
       fontSize: size,
     }}
   >
-    <p>
-      <FontAwesome name="user" />
-      &nbsp;
-      <b>{ question.user }</b>
-    </p>
-    <p>{ question.text }</p>
+    <Grid>
+      <Row className="show-grid">
+        <Col xs={12} md={8}>{
+          <div>
+            <p>
+              <FontAwesome name="user" />
+                &nbsp;
+              <b>{ question.user }</b>
+            </p>
+            <p>
+                &nbsp;
+                &nbsp;
+              { question.text }
+            </p>
+            <Button
+              onClick={changeLook}
+            >
+                Like
+                &nbsp;
+                <FontAwesome name="thumbs-up" />
+            </Button>
+            <Button
+              onClick={changeLook}
+            >
+                Reply
+                &nbsp;
+                <FontAwesome name="reply" />
+            </Button>
+          </div>}</Col>
+        <Col xs={6} md={4}>{
+          <p>
+            <FontAwesome name="clock-o" />
+                &nbsp;
+              TimeAndDate
+          </p>}</Col>
+
+      </Row>
+    </Grid>
   </div>
 );
 QuestionItem.propTypes = {
+  question: React.PropTypes.string.isRequired,
   color: React.PropTypes.string.isRequired,
   size: React.PropTypes.string.isRequired,
 };
