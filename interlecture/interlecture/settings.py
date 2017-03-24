f2b797bd5d87819b3143a5d3937e75070c282707
Kristian Trandem <kkrorkk@gmail.com>
2017-03-24 15:16:56 +0100

    add <www.>interlecture.no to ALLOWED_HOSTS

--- interlecture/interlecture/settings.py
+++ interlecture/interlecture/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['interlecture.no', 'www.interlecture.no']
 
 # Application definition
 
