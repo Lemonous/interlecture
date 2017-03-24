9c24edc06525e6c2b93199c093507241b536212d
Kristian Trandem <kkrorkk@gmail.com>
2017-03-24 14:52:55 +0100

    fix react proptypes and modify classroomtest to make it run smoothly again

--- interlecture/client/__tests__/classroom.react-test.js
+++ interlecture/client/__tests__/classroom.react-test.js
@@ -7,26 +7,31 @@ const questions = [
     id: 1,
     user: 'Rolf',
     body: 'hvorfor gjør vi dette?',
+    datetime: 'whatever',
   },
   {
     id: 2,
     user: 'Arne',
     body: 'kommer dette på eksamen?',
+    datetime: 'whatever',
   },
   {
     id: 3,
     user: 'Kåre',
     body: 'hva er meningen med livet?',
+    datetime: 'whatever',
   },
   {
     id: 4,
     user: 'Bjarne',
     body: 'finnes egentlig julenissen?',
+    datetime: 'whatever',
   },
   {
     id: 5,
     user: 'Rolf',
     body: 'jeg skjønner ikke håndskriften din :(',
+    datetime: 'whatever',
   },
 ];
 
@@ -43,7 +48,13 @@ const classroom = {
 
 test('Classroom looks like it should', () => {
   const component = renderer.create(
-    <Classroom classroom={classroom} submitQuestion={onSubmit} questions={questions} />);
+    <Classroom
+      classroom={classroom}
+      submitQuestion={onSubmit}
+      submitReply={onSubmit}
+      submitLike={onSubmit}
+      questions={questions}
+    />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
