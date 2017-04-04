18aa90e68904c2d41408fbd4b3e4e79b60354680
plotkin1996 <plotkin@null.net>
2017-04-04 14:19:41 +0200

    Fixed postid on client side.

--- interlecture/client/core/reducers.jsx
+++ interlecture/client/core/reducers.jsx
@@ -3,22 +3,19 @@ import { CLICK_REPLY } from './actions';
 function posts(state = [], action) {
   switch (action.type) {
     case 'NEW_POSTS':
-      var result=[...state];
-      for(var id in action.data) {
-          result[action.data[id].id]=action.data[id];
+      var result=state.slice();
+      for(var n in action.data) {
+          result[action.data[n].id]=action.data[n];
         }
       return result;
     
     case 'DELETE_POST':
-      var result=[...state];
-      result.splice(action.post,1);
-      return result;
+      return state.filter(post => post.id!=action.post);
       
 
     case CLICK_REPLY:
-      return [
-        ...state,
-      ];
+      return state.slice();
+      
     default:
       return state;
   }
