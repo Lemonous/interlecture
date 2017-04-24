fa4cfca3e970118e20a27282f4923c62af3af1ac
afriestad <afriestad@users.noreply.github.com>
2017-04-24 21:56:06 +0200

    Retry codecov.yaml

--- interlecture/interlecture/routing.py
+++ interlecture/interlecture/routing.py
@@ -1,8 +1,6 @@
 from channels.routing import route,include
 import engine.consumers
 
-# codecov ignore start
 channel_routing = [
     include(engine.consumers.channel_routing)
   ]
-# codecov ignore end
--- interlecture/interlecture/wsgi.py
+++ interlecture/interlecture/wsgi.py
@@ -6,7 +6,6 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
-# codecov ignore start
 
 import os
 
@@ -15,5 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "interlecture.settings")
 
 application = get_wsgi_application()
-
-# codecov ignore end
