a30251cd94ca1e4dcfbd14cc2abed329c49ca1ac
Kristian Trandem <kkrorkk@gmail.com>
2017-04-25 11:56:17 +0200

    test the CourseSelect component

--- interlecture/client/courses/courseSelect.jsx
+++ interlecture/client/courses/courseSelect.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { render } from 'react-dom';
 import Header from 'client/core/header';
 
 import * as bs from 'react-bootstrap';
 
-const Main = () => (
+const CourseSelect = () => (
   <div
     style={{
       display: 'flex',
@@ -15,36 +14,40 @@ const Main = () => (
       flexDirection: 'column',
     }}
   >
-    <Header user={window.django2react.username}
-            firstname={window.django2react.firstname}
-            lastname={window.django2react.lastname}
+    <Header
+      user={window.django2react.username}
+      firstname={window.django2react.firstname}
+      lastname={window.django2react.lastname}
     />
 
     {window.django2react.doesNotExist &&
-    <bs.Panel className={'message-panel'}>
-      <p style={{ marginBottom: '15px' }}>No such course exists</p>
-    </bs.Panel>
-    }
+      <bs.Panel className={'message-panel'}>
+        <p style={{ marginBottom: '15px' }}>No such course exists</p>
+      </bs.Panel>}
     {window.django2react.justLoggedIn &&
-    <bs.Panel className={'message-panel'}>
-      <p style={{ marginBottom: '15px' }}>Welcome to Interlecture, {window.django2react.username}</p>
-    </bs.Panel>
-    }
+      <bs.Panel className={'message-panel'}>
+        <p style={{ marginBottom: '15px' }}>
+          Welcome to Interlecture, {window.django2react.username}
+        </p>
+      </bs.Panel>}
     <bs.Panel>
       <bs.Form action="/courses/" method="post">
         <bs.FormGroup>
           <bs.FormControl
-            type="text" name="name" defaultValue={window.django2react.courseName}
+            type="text"
+            name="name"
+            defaultValue={window.django2react.courseName}
             placeholder="Course code"
           />
         </bs.FormGroup>
         <bs.FormGroup>
-          <bs.Button className={'single'} bsStyle="primary" type="submit" block>Go to course</bs.Button>
+          <bs.Button className={'single'} bsStyle="primary" type="submit" block>
+            Go to course
+          </bs.Button>
         </bs.FormGroup>
         <input type="hidden" name="csrfmiddlewaretoken" value={window.django2react.csrfToken} />
       </bs.Form>
     </bs.Panel>
   </div>
 );
-
-render(<Main />, document.getElementById('main'));
+export default CourseSelect;
